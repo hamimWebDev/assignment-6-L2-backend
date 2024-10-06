eaed7fcc39e7fffa93ad40de15941547de0b5f06
hamimDev <mdhamimhowladerasif@gmail.com>
2024-10-06 17:59:55 -0600

    search and infinity scroll done

--- src/app/modules/Recipes/recipes.service.ts
+++ src/app/modules/Recipes/recipes.service.ts
@@ -4,6 +4,7 @@ import { User } from '../Auth/auth.model'
 import { IRecipe } from './recipes.interface'
 import { Recipe } from './recipes.model'
 import { JwtPayload } from 'jsonwebtoken'
+import QueryBuilder from '../../builder/QueryBuilder'
 
 const createRecipesIntoDb = async (payload: IRecipe) => {
   const { author } = payload
@@ -15,21 +16,33 @@ const createRecipesIntoDb = async (payload: IRecipe) => {
   return result
 }
 
-const getAllRecipes = async (user: JwtPayload) => {
-  let result
+const getAllRecipes = async (
+  user: JwtPayload,
+  query: Record<string, unknown>,
+) => {
+  let baseQuery = Recipe.find({ isDeleted: false, isPublished: true })
 
-  if (user?.role === 'admin') {
-    result = await Recipe.find({ isDeleted: false, isPublished: true })
-  } else if (user?.isPremium) {
-    result = await Recipe.find({ isDeleted: false, isPublished: true })
-  } else {
-    result = await Recipe.find({
-      isDeleted: false,
-      isPublished: true,
-      isPremium: false,
-    }) // Only return non-premium recipes
+  // Apply restrictions for non-premium users
+  if (!user?.isPremium && user?.role !== 'admin') {
+    baseQuery = baseQuery.find({ isPremium: false })
   }
-  return result
+
+  // Initialize the QueryBuilder with the base query and the user's query parameters
+  const queryBuilder = new QueryBuilder(baseQuery, query)
+
+  // Use QueryBuilder for advanced search, filtering, sorting, and pagination
+  const resultQuery = queryBuilder
+    .search(['title', 'tags', 'ingredients.name']) // Searchable fields: title, tags, ingredients
+    .filter() // Apply filters from query
+    .sort() // Apply sorting if any
+    .paginate() // Apply pagination
+    .fields().modelQuery // Select specific fields if required // Get the built query
+
+  // Execute the final query
+  const recipes = await resultQuery
+  const totalData = await queryBuilder.countTotal() // Optional: Get total counts for pagination
+
+  return { recipes, totalData }
 }
 
 const getRecipeById = async (id: string) => {
@@ -47,7 +60,7 @@ const updateRecipeById = async (id: string, payload: Partial<IRecipe>) => {
   const recipe = await Recipe.findById(id)
   if (!recipe) {
     throw new AppError(httpStatus.NOT_FOUND, 'Recipe is not found')
-  };
+  }
 
   const result = await Recipe.findByIdAndUpdate(id, payload, { new: true })
   return result
@@ -63,5 +76,5 @@ export const RecipeServices = {
   deleteRecipesFromDb,
   getAllRecipes,
   getRecipeById,
-  updateRecipeById
+  updateRecipeById,
 }
